25d8cf53a189cc99cd580bcfc5ad39f006678a92
FelRod-Diecast <nnbwb8g579@privaterelay.appleid.com>
2026-09-02 16:36:10 -0500

    Fix typo in variable declaration for shipIndex

--- bot.js
+++ bot.js
@@ -1012,7 +1012,7 @@ console.log(
     /Launches\s+[A-Za-z]+\s+\d{1,2},\s+\d{4}\s+\d{1,2}:\d{2}\s+[ap]m\s+PT/i
   );
 
-cconst shipIndex =
+const shipIndex =
   html.indexOf("Ships on or before");
 
 console.log(
